0d1c1c9828a7b06dc337c63463cdfdc1b008689f
Tamer Maher <tmaher1982@gmail.com>
2021-07-28 01:15:11 +0200

    Implementing the TableBoxListModel

--- Source/PlaylistComponent.cpp
+++ Source/PlaylistComponent.cpp
@@ -11,13 +11,20 @@
 #include <JuceHeader.h>
 #include "PlaylistComponent.h"
 
+
 //==============================================================================
 PlaylistComponent::PlaylistComponent()
 {
     // In your constructor, you should add any child components, and
     // initialise any special settings that your component needs.
+    
+    trackTitles.push_back("Track 1");
+    trackTitles.push_back("Track 2");
+    
     tableComponent.getHeader().addColumn("Track title", 1, 400); // Making 1 instead of 0 for juce 6
     tableComponent.getHeader().addColumn("Artist", 2, 400);
+    tableComponent.setModel(this);
+    
     addAndMakeVisible(tableComponent);
 }
 
@@ -50,4 +57,19 @@ void PlaylistComponent::resized()
     // This method is where you should set the bounds of any child
     // components that your component contains..
     tableComponent.setBounds(0,0,getWidth(), getHeight());
+}
+
+int PlaylistComponent::getNumRows()
+{
+    return trackTitles.size();
+}
+
+void PlaylistComponent::paintRowBackground (Graphics &, int rowNumber, int width, int Height, bool rowIsSelected)
+{
+    
+}
+
+void PlaylistComponent::paintCell (Graphics &, int rowNumber, int columnId, int width, int Height, bool rowIsSelected)
+{
+    
 }
--- Source/PlaylistComponent.h
+++ Source/PlaylistComponent.h
@@ -11,11 +11,15 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include <vector>
+#include <string>
+
+using namespace juce;
 
 //==============================================================================
 /*
 */
-class PlaylistComponent  : public juce::Component
+class PlaylistComponent  : public juce::Component, public juce::TableListBoxModel
 {
 public:
     PlaylistComponent();
@@ -23,9 +27,15 @@ public:
 
     void paint (juce::Graphics&) override;
     void resized() override;
+    int getNumRows() override;
+    
+    void paintRowBackground (Graphics &, int rowNumber, int width, int Height, bool rowIsSelected) override;
 
+    void paintCell (Graphics &, int rowNumber, int columnId, int width, int Height, bool rowIsSelected) override;
 private:
     
     juce::TableListBox tableComponent;
+    std::vector<std::string> trackTitles;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlaylistComponent)
 };
